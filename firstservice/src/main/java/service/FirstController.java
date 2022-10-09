708e5a0a3d8fdf4f1f75751ea8c94e83f372afca
Ahn <abu89@naver.com>
2022-10-09 13:12:32 +0900

    [CLOUD] Load balance
    + register gateway to eureka discovery service
    + change cloud route to load balance uri

--- firstservice/src/main/java/service/FirstController.java
+++ firstservice/src/main/java/service/FirstController.java
@@ -1,15 +1,23 @@
 package service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
+@RequiredArgsConstructor
 public class FirstController {
+
+    private final Environment env;
 
     @GetMapping("/home")
     public String home() {
@@ -23,7 +31,9 @@ public class FirstController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, this is first service";
+    public String check(HttpServletRequest request) {
+        String serverPort = env.getProperty("local.server.port");
+        log.info("env local server port = {} ", serverPort);
+        return "Hi, this is first service, port is " + request.getServerPort();
     }
 }
